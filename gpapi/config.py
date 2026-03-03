f6a4816d74437b66398a59792d8b7d982c96e51a
Akhil <akhil@e.email>
2026-03-03 18:38:21 +0530

    chore: cleanup extra newline

--- gpapi/config.py
+++ gpapi/config.py
@@ -151,6 +151,7 @@ class DeviceBuilder(object):
                 "client_sig": "38918a453d07199354f8b19af05ec6562ced5788",
                 "callerSig": "38918a453d07199354f8b19af05ec6562ced5788",
                 "sdk_version": self.device.get('build.version.sdk_int')}
+
     def getAndroidCheckinRequest(self):
         request = googleplay_pb2.AndroidCheckinRequest()
         request.id = 0
