465705200a2d5f55eabc583ffd21ce102ec6db72
Akhil <akhil@e.email>
2026-08-17 16:59:03 +0530

    fix: add server-side error to handle 5xx codes properly

--- gpapi/googleplay.py
+++ gpapi/googleplay.py
@@ -58,7 +58,7 @@ class TokenExpiredError(Exception):
     def __str__(self):
         return repr(self.value)
 
-class TokenNotFoundError(Exception):
+class ServerSideError(Exception):
     def __init__(self, value):
         self.value = value
 
@@ -234,5 +234,8 @@ class GooglePlayAPI(object):
             self.setAuthSubToken(params["auth"])
         elif "error" in params:
             raise TokenExpiredError("server says: " + params["error"])
+        elif response.status_code // 100 == 5:
+            raise ServerSideError("Google server returned %d: %s" % (response.status_code,
+                                                              response.text))
         else:
-            raise TokenNotFoundError("Auth token not found. Response: %s" % response.text)
+            raise LoginError("Auth token not found. Response: %s" % response.text)
